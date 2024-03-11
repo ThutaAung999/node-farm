ddaa96e51154f3be4e946423126035a662d0f881
ThutaAung999 <exercise999123@gmail.com>
2024-03-11 23:03:31 +0630

    update

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ console.log('Will read file!');
 
 ///////////////
 //Server
+
+const replaceTemplate=(temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+
+
 const tempOverview = fs.readFileSync(
     `${__dirname}/templates/template-overview.html`,
     'utf-8'
@@ -71,8 +87,13 @@ const server = http.createServer((req, res) => {
             'Content-type': 'text/html'
           });
 
-        res.end(tempOverview);
+          const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); 
+          //console.log(cardsHtml);
+
+          const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
     }
+    
     //Product Page
     else if(pathName==='/product'){
         res.end('This is the Product');
