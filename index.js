04f253d6bf61d801eaf14497aa7c56a8afe1c688
ThutaAung999 <exercise999123@gmail.com>
2024-03-12 11:30:56 +0630

    complete section -2 of  Node and Express js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs=require('fs')
 const http = require('http');
 const url=require('url');
 
+const slugify=require('slugify');
+
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ///////////////
 //File
 
@@ -40,19 +44,6 @@ console.log('Will read file!');
 ///////////////
 //Server
 
-const replaceTemplate=(temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-}
 
 
 const tempOverview = fs.readFileSync(
@@ -73,6 +64,10 @@ const tempOverview = fs.readFileSync(
 const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj= JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+
 const server = http.createServer((req, res) => {
     //console.log("Request object:",req);
     
@@ -80,7 +75,6 @@ const server = http.createServer((req, res) => {
 
     const { query, pathname } = url.parse(req.url, true);
     
-
     //Overview Page
     if(pathname==='/' || pathname==='/overview'){ 
 
@@ -129,3 +123,4 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
   });
+
