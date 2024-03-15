7850af09f5d31157db6477e45c43915337de59d5
ThutaAung999 <exercise999123@gmail.com>
2024-03-15 12:46:59 +0630

    Before refactoring (57)

--- natours/app.js
+++ natours/app.js
@@ -37,6 +37,27 @@ app.get('/api/v1/tours',(req,res) => {
     })
 });
 
+app.get('/api/v1/tours/:id',(req,res) => {
+    console.log(req.params);
+
+    const id=req.params.id*1;
+    const tour=tours.find(el=>el.id===id)
+
+    if(!tour){
+        return res.status(404).json({
+            status:'Error',
+            message:'Invalid ID provided'
+        })
+    }
+    
+    res.status(200).json({
+        status:'Success',
+        data:{
+            tour
+        }   
+    });
+});
+
 
 app.post('/api/v1/tours', (req, res) => {
     //console.log(req.body);
@@ -55,6 +76,45 @@ app.post('/api/v1/tours', (req, res) => {
         })
     })    
 });
+
+
+app.patch('/api/v1/tours/:id',(req,res) => {
+
+    if(req.params.id*1>tours.length){
+        return res.status(404).json({
+            status:'Fail',
+            message:'Invalid ID provided'
+        });
+    }
+
+    res.status(200).json({
+        status:'Success',
+        data:{
+            tour:'<Updated Tour Here>'
+        }    
+});
+
+})
+
+
+
+app.delete('/api/v1/tours/:id',(req,res) => {
+
+    if(req.params.id*1>tours.length){
+        return res.status(404).json({
+            status:'Fail',
+            message:'Invalid ID provided'
+        });
+    }
+
+    res.status(204).json({
+        status:'Success',
+        data:null
+});
+
+})
+
+
 
 
 const port=3000;
