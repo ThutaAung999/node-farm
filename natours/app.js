115d40879e4344426dd7f46f06a47fa8bac8d647
ThutaAung999 <exercise999123@gmail.com>
2024-03-17 17:50:59 +0630

    end of Section 6

--- natours/app.js
+++ natours/app.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-console.log(process.env.NODE_ENV)
-if(process.env.NODE_ENV === 'development'){
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
-
-
-
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
 
 app.use(express.static(`${__dirname}/public`));
 
