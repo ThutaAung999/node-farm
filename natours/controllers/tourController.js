624ed4b8803b6da27548731f1f7f07957b782c30
ThutaAung999 <exercise999123@gmail.com>
2024-03-19 13:27:34 +0630

    88-Refactor to MVC

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,10 +1,8 @@
 const fs = require("fs");
+const Tour=require("./../model/tourModel");
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
 
-exports.checkID = (req, res, next, val) => {
+/* exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is : ${val} `);
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
@@ -13,7 +11,7 @@ exports.checkID = (req, res, next, val) => {
     });
   }
   next();
-};
+}; */
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -32,10 +30,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "Success",
     requestedAt: req.requestTime,
-    results: tours.length,
+    /* results: tours.length,
     data: {
       tours,
-    },
+    }, */
   });
 };
 
@@ -43,45 +41,23 @@ exports.getTour = (req, res) => {
   console.log(req.params);
 
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  /* const tour = tours.find((el) => el.id === id);
   //console.log(tour);
 
   return res.status(200).json({
     status: "Success",
     data: { tour },
-  });
+  }); */
 };
 
 exports.createTour = (req, res) => {
   //console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      
-
-        if (err) {
-            // Handle error
-            console.log(err);
-            return res.status(500).json({
-                status: 'Error',
-                message: 'Failed to create tour'
-            });
-        }
-
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: "Success"
+    /* data: {
+      tour: newTour
+    } */
+   });
 };
 
 exports.updateTour = (req, res) => {
--- natours/server.js
+++ natours/server.js
@@ -12,45 +12,13 @@ const DB = process.env.DATABASE.replace(
 mongoose
 //  .connect(process.env.DATABASE_LOCAL, {
   .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true // Add this line to use the new server discovery and monitoring engine
-    
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true // Add this line to use the new server discovery and monitoring engine    
   })
   .then((con) =>{
     //console.log(con.connections) 
     console.log('DB connection successful!')})
   .catch(err => console.error('DB connection error:', err)); // Add error handling
-
-const tourSchema=new mongoose.Schema({
-  name:{
-    type:String,
-    required:[true,'Tour must have a  name'],
-    unique:true
-  },
-  rating :{
-    type:Number,
-    default:4.5
-  },
-  price:{
-    type:Number,
-    required:[true,'Tour must have a price']
-  }
-});
-
-const Tour=mongoose.model('Tour',tourSchema);
-
-
-const testTour=new Tour({
-  name:'The Part Camper',
-  price:997
-});
-
-
-testTour.save().then(doc=>{
-  console.log(doc);
-}).catch(err=>{
-  console.log("Error: " , err);
-});
 
   const port = process.env.PORT || 3000;
 app.listen(port, () => {
