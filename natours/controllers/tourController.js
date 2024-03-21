5995915a27581d4095d87f99ca0c4f9e19304f8e
ThutaAung999 <exercise999123@gmail.com>
2024-03-21 10:40:42 +0630

    100- Before refactoring API

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,49 +1,83 @@
 const fs = require('fs');
 const Tour = require('./../model/tourModel');
 
-/* exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is : ${val} `);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid ID provided",
-    });
-  }
-  next();
-}; */
-
-/* exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Missing name or price",
-    });
-  }
+exports.aliasTopTours= (req, res, next) => {
+  req.query.limit='5';
+  req.query.sort='-ratingAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary,difficulty';
   next();
 };
- */
+
+class APIFeatures{
+	constructor(query, queryString){
+      this.query = query;
+      this.queryString = queryString;
+  }
+
+  filter(){
+
+    
+  }
+}
+
 
 exports.getAllTours = async (req, res) => {
   try {
-
-  //BUILD THE QUERY  
-  //1)Filtering
+    //BUILD THE QUERY  
+    //1A)Filtering
   
     const queryObj  = { ...req.query};    
     const excludedFields=['page','sort','limit','fields'];
-    excludedFields.forEach(el=> delete queryObj[el]);
-        
+    excludedFields.forEach(el=> delete queryObj[el]);        
     
-    //2 Advanced filtering
+    //1B Advanced filtering
     let queryStr=JSON.stringify(queryObj);    
     queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match=>`$${match}`);
-    console.log(" JSON.parese(queryStr) :   " , JSON.parse(queryStr)); 
+    //console.log(" JSON.parese(queryStr) :   " , JSON.parse(queryStr)); 
 
-    const query= Tour.find( JSON.parse(queryStr));
+    let query= Tour.find( JSON.parse(queryStr));
+
+    //2-Sorting 
+    if(req.query.sort){
+      /* 
+        query=query.sort(req.query.sort);
+        //sort('price  ratingAverage')    
+     */
+
+        const sortBy= req.query.sort.split(',').join(' ');
+        //console.log(sortBy);
+        query=query.sort(sortBy);      
+    }else{
+      query=query.sort('-createdAt');
+    }
+
+
+    //3 Fields Limiting
+
+    if(req.query.fields){
+      const fields= req.query.fields.split(',').join(' ');
+      //console.log(fields);
+      query=query.select(fields);
+    }else{
+      query=query.select('-__v');
+    }
+
+
+    //4) Pagination
+    const page=req.query.page*1|| 1 ;
+    const limit=req.query.limit * 1 || 100 ;
+    const skip = (page-1)*limit;
+
+    //page=2&limit=10
+    query=query.skip(skip).limit(limit);
+
+    if(req.query.page){
+      const numTours= await Tour.countDocuments();
+      if(skip>=numTours)throw new Error("This page does not exist");
+    }
 
     //Execute the query
     const tours=await  query;
-
     console.log("tours",tours);
 
     res.status(200).json({
