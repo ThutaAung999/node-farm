2ac00ef903886e7b2e65f3a77d32a217d26b7918
ThutaAung999 <exercise999123@gmail.com>
2024-03-26 11:09:45 +0630

    130- Successful signup and login

--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -6,6 +6,7 @@ const authController = require('./../controllers/authController');
 const router = new express.Router();
 
 router.post('/signup',authController.signup);
+router.post('/login', authController.login);
 
 router.route('/')
 .get(userController.getAllUsers)
