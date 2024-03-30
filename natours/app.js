7afff03cc72fd96a4160485ede99ed1a3b982d09
ThutaAung999 <exercise999123@gmail.com>
2024-03-30 22:24:50 +0630

    data sanitization

--- natours/app.js
+++ natours/app.js
@@ -5,13 +5,20 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+const mongoSanitize = require('express-mongo-sanitize');
 const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+
+const helmet = require('helmet');
 
 
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+//Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -25,14 +32,24 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+//Body parser , reading data from body into req.body
+app.use(express.json({limit:'10kb'}));
 
-app.use(express.json());
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+//Serving staic files
 app.use(express.static(`${__dirname}/public`));
 
+
+//Test middleware
 app.use((req, res, next) => {
-  console.log("custom middleware to print current time")
+  //console.log("custom middleware to print current time")
   req.requestTime = new Date().toISOString();
-console.log("req.headers:",req.headers);  
+//console.log("req.headers:",req.headers);  
   next();
 });
 
