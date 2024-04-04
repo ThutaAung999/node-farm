cbd18bd6c07128e8422f9b86f43a80c7a9deadc0
ThutaAung999 <exercise999123@gmail.com>
2024-04-04 11:01:40 +0630

    181- set up project structure

--- natours/app.js
+++ natours/app.js
@@ -12,7 +12,7 @@ const rateLimit = require('express-rate-limit');
 const xss = require('xss-clean');
 
 const reviewRouter = require('./routes/reviewRoutes');
-
+const viewRouter = require('./routes/viewRoutes');
 
 
 const hpp=require('hpp');
@@ -80,28 +80,7 @@ app.use((req, res, next) => {
 
 
 // 3) ROUTES
-app.get('/',(req,res) => {
-  res.status(200).render('base',{
-    tour:'The Forest Hiker',
-    user:'Htein'
-  });
-})
-
-
-
-app.get('/overview',(req,res) => {
-  res.status(200).render('overview',{
-    title: " All Tours"
-  })
-})
-
-app.get('/tour',(req,res) => {
-  res.status(200).render('tour',{
-    title: "The Forest Hiker Tour"
-  })
-})
-
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);//Prod အတွက် middleware မှာ error တက်နေ
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
