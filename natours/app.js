41111f63d57354e01eada721d480b3fd5b55c98b
ThutaAung999 <exercise999123@gmail.com>
2024-04-04 10:24:12 +0630

    180- Extending our base templates with blocks

--- natours/app.js
+++ natours/app.js
@@ -88,6 +88,20 @@ app.get('/',(req,res) => {
 })
 
 
+
+app.get('/overview',(req,res) => {
+  res.status(200).render('overview',{
+    title: " All Tours"
+  })
+})
+
+app.get('/tour',(req,res) => {
+  res.status(200).render('tour',{
+    title: "The Forest Hiker Tour"
+  })
+})
+
+
 app.use('/api/v1/tours', tourRouter);//Prod အတွက် middleware မှာ error တက်နေ
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
