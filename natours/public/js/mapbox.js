80ea7f83cd5e3190447a77f36f3a9f83dfe4fc0b
ThutaAung999 <exercise999123@gmail.com>
2024-04-06 12:35:32 +0630

    188 building the login screen

--- natours/public/js/mapbox.js
+++ natours/public/js/mapbox.js
@@ -2,3 +2,11 @@
 
 const locations= JSON.parse(document.getElementById('map').dataset.locations)
 console.log(locations)
+
+maptilersdk.config.apiKey = 'IAd8rvnqXUMsACg2wBWS';
+const map = new maptilersdk.Map({
+  container: 'map', // container's id or the HTML element in which the SDK will render the map
+  style: maptilersdk.MapStyle.STREETS,
+  center: [16.62662018, 49.2125578], // starting position [lng, lat]
+  zoom: 14 // starting zoom
+});
