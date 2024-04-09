4855366300db3eed0cae640bbcade3f8022aa350
ThutaAung999 <exercise999123@gmail.com>
2024-04-09 14:45:40 +0630

    200- configuring multer

--- natours/controllers/handlerFactory.js
+++ natours/controllers/handlerFactory.js
@@ -18,7 +18,7 @@ exports.deleteOne = Model =>
       data: null
     });
   });
-/* 
+
 exports.updateOne = Model =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
@@ -37,10 +37,10 @@ exports.updateOne = Model =>
       }
     });
   });
- */
+
 
   
-  exports.updateOne = Model =>
+ /*  exports.updateOne = Model =>
    catchAsync(async (req, res, next) => {
      const doc = await Model.findById(req.params.id);
 
@@ -65,7 +65,7 @@ exports.updateOne = Model =>
    });
 
 
-
+ */
 exports.createOne = Model =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body);
--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -5,11 +5,9 @@ const authController = require('./../controllers/authController');
 
 const router = new express.Router();
 
-const multer = require('multer');
+//const multer = require('multer');
 
-const upload = multer({dest: 'public/img/users'})
-
-
+//const upload = multer({dest: 'public/img/users'})
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
@@ -24,7 +22,8 @@ router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', upload.single('photo'),userController.updateMe);
+router.patch('/updateMe',userController.uploadUserPhoto,userController.updateMe);
+  
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
