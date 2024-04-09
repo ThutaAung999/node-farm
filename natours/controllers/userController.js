c7bca1f1f99f3eb66761e9172d200ff1a5cf862a
ThutaAung999 <exercise999123@gmail.com>
2024-04-09 15:08:34 +0630

    201-saving image name to DB

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -47,8 +47,9 @@ const filterObj = (obj, ...allowedFields) => {
 
   
 exports.updateMe = catchAsync(async (req, res, next) => {
-    console.log('req.file', req.file);
-    console.log('req.body', req.body);
+    //console.log('req.file', req.file);
+    //console.log('req.body', req.body);
+    
     // 1) Create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
       return next(
