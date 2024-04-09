001fa2aadbae1e396909cf168192c106438316cb
ThutaAung999 <exercise999123@gmail.com>
2024-04-09 09:33:30 +0630

    196 update use data with our API

--- natours/public/js/index.js
+++ natours/public/js/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { login ,logout} from './login';
+import { updateSettings } from './updateSettings';
 
 //const loginForm = document.querySelector('.form--login');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
-
+const userDataForm = document.querySelector('.form-user-data');
 
 if (loginForm)
   loginForm.addEventListener('submit', (e) => {
@@ -18,3 +19,13 @@ if (loginForm)
 
 
   if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+  if (userDataForm)
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings( name, email );
+  });
+
+
