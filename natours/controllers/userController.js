3c393c0feb9865e9334821ace7958d77dc9cf138
ThutaAung999 <exercise999123@gmail.com>
2024-04-09 15:44:41 +0630

    202- resizing image

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -1,12 +1,14 @@
 const multer = require('multer');
 
+const sharp= require('sharp');
+
 const User = require('./../model/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory= require('./handlerFactory');
 
 
- const multerStorage = multer.diskStorage({
+/*  const multerStorage = multer.diskStorage({
    destination: (req, file, cb) => {
      cb(null, 'public/img/users');
    },
@@ -15,8 +17,10 @@ const factory= require('./handlerFactory');
      cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
    }
  });
+ */
 
-
+ const multerStorage = multer.memoryStorage();
+ 
  const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
@@ -31,6 +35,23 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto= upload.single('photo');
+
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
+
+
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -49,7 +70,7 @@ const filterObj = (obj, ...allowedFields) => {
 exports.updateMe = catchAsync(async (req, res, next) => {
     //console.log('req.file', req.file);
     //console.log('req.body', req.body);
-    
+
     // 1) Create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
       return next(
